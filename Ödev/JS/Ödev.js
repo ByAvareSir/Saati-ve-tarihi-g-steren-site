f2c0b68330705ffdb36483e9392cc5f7dfcf8cce
Ömer Faruk Tamgaç <99759641+ByAvareSir@users.noreply.github.com>
2022-08-09 01:18:48 +0300

    Add files via upload

---
+++ Ödev/JS/Ödev.js
@@ -0,0 +1,28 @@
+let name=prompt("İsminizi giriniz:");
+
+
+document.getElementById('ad').innerHTML= name+",";
+
+function tarihsaat(){
+var tarih=new Date();
+var saat=tarih.getHours();
+var dakika=tarih.getMinutes();
+var saniye=tarih.getSeconds();
+var yıl=tarih.getFullYear();
+var ay=tarih.getMonth();
+var gün=tarih.getDay();
+
+const ayism = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos"
+,"Eylül","Ekim","Kasım","Aralık"];
+
+
+document.getElementById('saat').innerHTML=saat+":";
+document.getElementById('dakika').innerHTML=dakika+":";
+document.getElementById('saniye').innerHTML=saniye;
+document.getElementById('gün').innerHTML=gün;
+document.getElementById('ay').innerHTML=ayism[ay];
+document.getElementById('yıl').innerHTML=yıl;
+
+};
+
+setInterval(tarihsaat,1000);
